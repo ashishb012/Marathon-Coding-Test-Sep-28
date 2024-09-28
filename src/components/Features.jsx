7a71702e327d57f1af81c6ac0748700f7e82acff
ashishb012 <ashishb0128@gmail.com>
2024-09-28 12:38:28 +0530

    Features

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
+import image5 from '../assets/chair5.jpg'
 
-const Features = () => {
+function Features () {
   return (
-    <div>Features</div>
+    <section className="features">
+      
+    <div className="features-content">
+      <h1 className="features-title">
+        <span className="light-grey-text">Best </span>
+        <span className="dark-grey-text">Features</span>
+      </h1>
+      <p className="features-description">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint, dolorem! Iste, debitis optio. Cupiditate sint exercitationem numquam repudiandae fugiat ullam omnis quis? Autem accusantium rerum dolorem nesciunt illo architecto quae.
+      </p>
+      <div className="features-grid-row">
+        <div className="features-item">
+            <h3>Longevity</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </div>
+        <div className="features-item">
+          <h3>Quality</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </div>
+        <div className="features-item">
+          <h3>Heritage</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </div>
+      </div>
+    </div>
+    <div className="features-image">
+      <img src={image5} alt="image5" className="rounded-full" />
+    </div>
+  </section>
   )
 }
 
--- src/components/WhyChooseUs.jsx
+++ src/components/WhyChooseUs.jsx
@@ -1,8 +1,41 @@
 import React from 'react'
+import image5 from '../assets/chair5.jpg'
 
 function WhyChooseUs () {
   return (
-    <div>WhyChooseUs</div>
+    <section className="features">
+      <div className="features-image">
+      <img src={image5} alt="image5" className="rounded-full" />
+    </div>
+    <div className="features-content">
+      <h2 className="features-title">
+        <span className="light-grey-text">Why Choose </span>
+        <span className="dark-grey-text">Us?</span>
+      </h2>
+      <p className="features-description">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint, dolorem! Iste, debitis optio. Cupiditate sint exercitationem numquam repudiandae fugiat ullam omnis quis? Autem accusantium rerum dolorem nesciunt illo architecto quae.
+      </p>
+      <div className="features-grid">
+        <div className="features-item">
+            <h3>Longevity</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </div>
+        <div className="features-item">
+          <h3>Quality</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </div>
+        <div className="features-item">
+          <h3>Heritage</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </div>
+        <div className="features-item">
+          <h3>Comfort</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </div>
+      </div>
+    </div>
+    
+  </section>
   )
 }
 
